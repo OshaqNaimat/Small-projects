f689c5110c578aac31181b1a360036becf75821c
oshaq naimat <oshaqnaimat15@gmail.com>
2025-11-22 10:15:27 +0500

    home page features

--- mini/src/pages/Home.jsx
+++ mini/src/pages/Home.jsx
@@ -10,9 +10,26 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <h2 className="text-center font-semibold text-2xl my-3">
-        My mini Projects
-      </h2>
+      <div className="flex justify-between p-3">
+        <a
+          href="https://github.com/OshaqNaimat"
+          target="_blank"
+          class="flex items-center px-6 py-3 bg-gray-800 hover:bg-gray-900 text-white font-bold rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105"
+        >
+          GitHub
+        </a>
+        <h2 className="text-center font-semibold text-2xl my-3">
+          My mini Projects
+        </h2>
+        <a
+          href="https://www.linkedin.com/in/oshaq-naimat-14347336a"
+          target="_blank"
+          class="flex items-center px-6 py-3 bg-gray-800 hover:bg-gray-900 text-white font-bold rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105"
+        >
+          Linkedin
+        </a>
+      </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 px-4">
         <div
           className="container flex justify-center items-center shadow-lg cursor-pointer  transform transition-transform duration-300 
