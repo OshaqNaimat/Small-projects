ff3b50bac6aecd350e211e73846e24e7a1296d00
oshaq naimat <oshaqnaimat15@gmail.com>
2025-11-22 10:02:35 +0500

    Home page

--- mini/src/pages/Home.jsx
+++ mini/src/pages/Home.jsx
@@ -11,31 +11,53 @@ export default function Home() {
   return (
     <>
       <h2 className="text-center font-semibold text-2xl my-3">
-        My small Projects
+        My mini Projects
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 px-4">
-        <div className="container shadow">
-          <Link to="/" className="project-card">
-            <FaHome size={32} />
-            <p>Home</p>
+        <div
+          className="container flex justify-center items-center shadow-lg cursor-pointer  transform transition-transform duration-300 
+            hover:-translate-y-2 rounded-md shadow-black p-10  "
+        >
+          <Link to="/lorem-generator" className="project-card">
+            <div className="flex justify-center">
+              <FaAlignLeft size={32} />
+            </div>
+            <p>Lorem Generator</p>
           </Link>
         </div>
-        <Link to="/lorem-generator" className="project-card">
-          <FaAlignLeft size={32} />
-          <p>Lorem Generator</p>
-        </Link>
-        <Link to="/color-generator" className="project-card">
-          <FaPalette size={32} />
-          <p>Color Generator</p>
-        </Link>
-        <Link to="/todo-list" className="project-card">
-          <FaTasks size={32} />
-          <p>To-Do List</p>
-        </Link>
-        <Link to="/birthday-reminder" className="project-card">
-          <FaBirthdayCake size={32} />
-          <p>Birthday Reminder</p>
-        </Link>
+        <div
+          className="container flex justify-center items-center shadow-lg cursor-pointer  transform transition-transform duration-300 
+            hover:-translate-y-2 rounded-md shadow-black p-10  "
+        >
+          <Link to="/color-generator" className="project-card">
+            <div className="flex justify-center">
+              <FaPalette size={32} />
+            </div>
+            <p>Color Generator</p>
+          </Link>
+        </div>
+        <div
+          className="container flex justify-center items-center shadow-lg cursor-pointer  transform transition-transform duration-300 
+            hover:-translate-y-2 rounded-md shadow-black p-10  "
+        >
+          <Link to="/todo-list" className="project-card">
+            <div className="flex justify-center">
+              <FaTasks size={32} />
+            </div>
+            <p>To-Do List</p>
+          </Link>
+        </div>
+        <div
+          className="container flex justify-center items-center shadow-lg cursor-pointer  transform transition-transform duration-300 
+            hover:-translate-y-2 rounded-md shadow-black p-10  "
+        >
+          <Link to="/birthday-reminder" className="project-card">
+            <div className="flex justify-center">
+              <FaBirthdayCake size={32} />
+            </div>
+            <p>Birthday Reminder</p>
+          </Link>
+        </div>
       </div>
     </>
   );
